2e19de67660fbe003f6359ccac9ab2e7a88f58be
BrunoSFavaro <bruno-silvafavaro@hotmail.com>
2024-05-03 23:00:28 -0300

    Correção de diretórios

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -19,38 +19,6 @@ class Main {
     $data = (new Scrapper())->scrap($dom);
 
     // Write your logic to save the output file bellow.
-    $xPath = new \DOMXPath($dom);
-
-    //Variáveis que irão extrair as informações do site usando XPath
-    $paperX = "//a[#class='paper-card p-lg bd-gradient-left']";
-    $titleX = ".//h4[@class='paper-title']";
-    $authorX = ".//div[@class='authors']/span";
-    $typeX = ".//div[@class='tags mr-sm']";
-    $idX = ".//div[@class='tags flex-row mr-sm']/div[@class='volume-info']";
-
-    // Juntando os dados dos diferentes arquivos
-    $papersNodes = $xPath->query("$paperX");
-    // Guardando os artigos em um vetor
-    $papersList = [];
-
-    // Foreach para percorrer cada artigo 
-    foreach ($papersNodes as $paperNode) {
-      $id = $xPath->query("$idX", $paperNode)->item(0)->nodeValue;
-      $title = $xPath->query("$titleX", $paperNode)->item(0)->nodeValue;
-      $type = $xPath->query("$typeX", $paperNode)->item(0)->nodeValue; 
-      
-      // Array vazia pela possibilidade de haver mais de um autor
-      $authors = [];
-      $authorsNodes = $xPath->query("$authorX", $paperNode);
-
-      // Foreach para extrair os autores 
-      foreach ($authorsNodes as $authorNode) {
-        $author = $authorNode->nodeValue;
-        $institution = $authorNode->getAttribute('title');
-          
-        $authors[] = new Person($author, $institution);
-        }
-      }
     }
 
   }
--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -14,17 +14,45 @@ class Scrapper {
    * Loads paper information from the HTML and returns the array with the data.
    */
   public function scrap(\DOMDocument $dom): array {
-    return [
-      new Paper(
-        123,
-        'The Nobel Prize in Physiology or Medicine 2023',
-        'Nobel Prize',
-        [
-          new Person('Katalin Karikó', 'Szeged University'),
-          new Person('Drew Weissman', 'University of Pennsylvania'),
-        ]
-      ),
-    ];
+    $xPath = new \DOMXPath($dom);
+
+    //Variáveis que irão extrair as informações do site usando XPath
+    $paperX = "//a[#class='paper-card p-lg bd-gradient-left']";
+    $titleX = ".//h4[@class='paper-title']";
+    $authorX = ".//div[@class='authors']/span";
+    $typeX = ".//div[@class='tags mr-sm']";
+    $idX = ".//div[@class='tags flex-row mr-sm']/div[@class='volume-info']";
+
+    // Juntando os dados dos diferentes arquivos
+    $papersNodes = $xPath->query("$paperX");
+    // Guardando os artigos em um vetor
+    $papersList = [];
+
+    // Foreach para percorrer cada artigo 
+    foreach ($papersNodes as $paperNode) {
+      $id = $xPath->query("$idX", $paperNode)->item(0)->nodeValue;
+      $title = $xPath->query("$titleX", $paperNode)->item(0)->nodeValue;
+      $type = $xPath->query("$typeX", $paperNode)->item(0)->nodeValue; 
+      
+      // Array vazia pela possibilidade de haver mais de um autor
+      $authors = [];
+      $authorsNodes = $xPath->query("$authorX", $paperNode);
+
+      // Foreach para extrair os autores 
+      foreach ($authorsNodes as $authorNode) {
+        $author = $authorNode->nodeValue;
+        $institution = $authorNode->getAttribute('title');
+        
+        $authors[] = new Person($author, $institution);
+      }
+      
+      // Instânciando o objeto com as informações extraídas 
+      $paper = new Paper($titleX, $authorX, $typeX, $idX);
+      $paperList[] = $paper;
+    }
+
+    // Retorna as informações extraídas dos artigos 
+    return $paperList;
   }
 
 }
