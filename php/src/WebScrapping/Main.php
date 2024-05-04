e6641ce719f6973c01b2100643be4d7ac8bbfb12
BrunoSFavaro <bruno-silvafavaro@hotmail.com>
2024-05-03 22:46:53 -0300

    Criado o lastro de repetição para percorrer os artigos

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace Chuva\Php\WebScrapping;
+use Chuva\Php\WebScrapping\Entity\Paper;
+use Person;
 
 /**
  * Runner for the Webscrapping exercice.
@@ -17,15 +19,32 @@ class Main {
     $data = (new Scrapper())->scrap($dom);
 
     // Write your logic to save the output file bellow.
-    $xpath = new \DOMXPath($dom);
+    $xPath = new \DOMXPath($dom);
 
     //Variáveis que irão extrair as informações do site usando XPath
-    $titleX = "//a/h4[@class='paper-title']";
-    $authorX = "//a/div[@class='authors']/span";
-    $typeX = "//a/div[@class='tags mr-sm']";
-    $idX = "//a/div[@class='tags flex-row mr-sm']/div[@class='volume-info']";
-    
-    print_r($data);
-  }
+    $paperX = "//a[#class='paper-card p-lg bd-gradient-left']";
+    $titleX = ".//h4[@class='paper-title']";
+    $authorX = ".//div[@class='authors']/span";
+    $typeX = ".//div[@class='tags mr-sm']";
+    $idX = ".//div[@class='tags flex-row mr-sm']/div[@class='volume-info']";
 
-}
+    // Juntando os dados dos diferentes arquivos
+    $papersNodes = $xPath->query("$paperX");
+    // Guardando os artigos em um vetor
+    $papersList = [];
+
+    // Foreach para percorrer cada artigo 
+    foreach ($papersNodes as $paperNode) {
+      $id = $xPath->query("$idX", $paperNode)->item(0)->nodeValue;
+      $title = $xPath->query("$titleX", $paperNode)->item(0)->nodeValue;
+      $type = $xPath->query("$typeX", $paperNode)->item(0)->nodeValue; 
+      
+      // Array vazia pela possibilidade de haver mais de um autor
+      $authors = [];
+      $authorsNodes = $xPath->query("$authorX", $paperNode);
+    }
+
+    // Retorna as informações extraídas dos artigos 
+    }
+
+  }
